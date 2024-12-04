db8d962c816e5154a64dede9964a7cb90c9b991b
“EternalDarknessCode” <mikelromerohomobono@gmail.com>
2024-12-04 11:46:17 +0100

    aniadido syso

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		
+		System.out.println("Prueba github");
 		//voy a crear un vehiculo
 		Vehiculo vehiculo;
 		String matricula; 
